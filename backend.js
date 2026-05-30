baa9745dd5301c67963a373b46ffebf00fc31796
Sean Behan <behan.s@northeastern.edu>
2026-05-30 19:09:17 -0400

    very basic express backend only loading static folder (like a client server two-tier architecture

---
+++ backend.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+console.log("What's up backend?");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// in express the way we serve static files is with the express app.use(express.static("folder-name"))
+app.use(express.static("frontend"));
+
+// second parameter is the function callback when the server is ready and running. 
+// The callback function you give will ONLY be called directly by express.
+// Since we are giving a callback function, we are not calling it anywere, Express will us that callback function when the server is ready and connected to the port
+app.listen(PORT, () => {
+  console.log(`Created a server listening on port ${PORT}`);
+});
